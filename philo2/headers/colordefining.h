3981021c67d0290fb9dd55df98f5b8a4750bec35
Tristan Goel <tgoel@c3r3s2.42lausanne.ch>
2022-07-22 14:53:18 +0200

    few changes

--- philo2/headers/colordefining.h
+++ philo2/headers/colordefining.h
@@ -6,19 +6,24 @@
 /*   By: tgoel <tgoel@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 13:02:15 by tgoel             #+#    #+#             */
-/*   Updated: 2022/07/22 14:19:38 by tgoel            ###   ########.fr       */
+/*   Updated: 2022/07/22 14:53:00 by tgoel            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef COLORDEFINING_H
 # define COLORDEFINING_H
 
-# define SCND "\e[0;32mnumber_of_philosophers"
+# define FRST "\n\tUsage: ./philo"
+# define SCND "\e[0;32m number_of_philosophers"
 # define THRD "\e[0;33m time_to_die"
 # define FRTH "\e[0;34m time_to_eat"
 # define FFTH "\e[0;35m time_to_sleep"
 # define SX "\e[0;36m [number_of_times_each_philosopher_must_eat]\e[0;37m"
 
-# define ALL SCND THRD FRTH FFTH SX
+# define P_FORKS "\e[0;35m Has taken a fork"
+# define P_SLEEP "\e[0;33m Is sleeping"
+# define P_EAT "\e[0;32 Is eating"
+
+# define ALL FRST SCND THRD FRTH FFTH SX
 
 #endif
