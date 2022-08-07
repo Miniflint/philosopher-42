41d28b6ad5440cd08dfd3a3a9a606f8f81f777e0
Tristan Goel <tgoel@c2r5s2.42lausanne.ch>
2022-08-07 08:25:53 +0200

    philo2

---
+++ philo2/src/main/eating.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   eating.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tgoel <tgoel@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/25 16:44:05 by tgoel             #+#    #+#             */
+/*   Updated: 2022/08/07 08:16:49 by tgoel            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../headers/philo.h"
+
+int	taking_fork(t_philo *philo)
+{
+	if (philo->backup->rules->died)
+		return (1);
+	if (pthread_mutex_lock(&philo->fork_right_id))
+		return (1);
+	if (pthread_mutex_lock(philo->fork_left_id))
+		return (1);
+	if (writing(philo, P_FORKS))
+		return (1);
+	if (writing(philo, P_FORKS))
+		return (1);
+	if (philo->backup->rules->died)
+		return (1);
+	return (0);
+}
+
+int	unlock_fork(t_philo *philo)
+{
+	if (philo->backup->rules->died)
+		return (1);
+	if (pthread_mutex_unlock(&philo->fork_right_id))
+		return (1);
+	if (pthread_mutex_unlock(philo->fork_left_id))
+		return (1);
+	if (philo->backup->rules->died)
+		return (1);
+	return (0);
+}
+
+int	eating(t_philo *philo)
+{
+	t_prog	*prog;
+
+	prog = philo->backup;
+	if (prog->rules->died)
+		return (1);
+	if (philo->last_meal + prog->rules->time_die
+		< (time_s() - prog->time_start))
+	{
+		if (writing(philo, "Is dead"))
+			return (1);
+		prog->rules->died = 1;
+		return (1);
+	}
+	if (taking_fork(philo))
+		return (1);
+	if (writing(philo, P_EAT))
+		return (1);
+	philo->ate += 1;
+	philo->last_meal = (time_s() - prog->time_start);
+	ft_usleep(philo->backup->rules->time_eat);
+	if (unlock_fork(philo))
+		return (1);
+	if (prog->rules->died)
+		return (1);
+	return (0);
+}
---
+++ philo2/src/utils/__init__.c
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   __init__.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tgoel <tgoel@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/17 06:19:00 by tgoel             #+#    #+#             */
+/*   Updated: 2022/08/07 07:47:56 by tgoel            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../headers/philo.h"
+
+static int	__init__time(t_prog *prog)
+{
+	prog->value_time = malloc(sizeof(struct timeval));
+	if (!prog->value_time)
+	{
+		handle_error("Error mallocing timeval");
+		return (1);
+	}
+	prog->time_start = time_s();
+	return (0);
+}
+
+static void	__init__philo(t_philo *philo, int amount_philo, t_prog *prog)
+{
+	int	i;
+
+	i = 0;
+	while (i < amount_philo)
+	{
+		philo[i].id = i;
+		philo[i].backup = prog;
+		philo[i].ate = 0;
+		philo[i].last_meal = 0;
+		pthread_mutex_init(&philo[i].fork_right_id, NULL);
+		i++;
+	}
+	i = 0;
+	while (i < amount_philo)
+	{
+		if (i == amount_philo - 1)
+			philo[i].fork_left_id = &philo[0].fork_right_id;
+		else
+			philo[i].fork_left_id = &philo[i + 1].fork_right_id;
+		i++;
+	}
+	pthread_mutex_init(&prog->writing, NULL);
+}
+
+static void	__init__rules(t_rules *rules, char **argv)
+{
+	rules->died = 0;
+	rules->nb_philo = ft_atoi(argv[1]);
+	rules->time_die = ft_atoi(argv[2]);
+	rules->time_eat = ft_atoi(argv[3]) * 1000;
+	rules->time_sleep = ft_atoi(argv[4]) * 1000;
+	if (rules->add_max_eat)
+		rules->nb_t_eat = ft_atoi(argv[5]);
+	else
+		rules->nb_t_eat = 0;
+}
+
+static int	create_size_t(t_prog *prog)
+{
+	prog->threads = malloc(sizeof(pthread_t) * prog->rules->nb_philo);
+	if (prog->threads)
+		return (1);
+	handle_error("Could not malloc threads");
+	return (0);
+}
+
+int	__init__(t_prog *prog, char **args, int max_eat)
+{
+	int			c_time;
+	t_philo		*philo;
+	t_rules		*rules;
+
+	rules = malloc(sizeof(t_rules));
+	if (!rules)
+		handle_error("Error mallocing: rules");
+	rules->add_max_eat = max_eat;
+	__init__rules(rules, args);
+	philo = malloc(sizeof(t_philo) * rules->nb_philo);
+	if (!philo)
+		handle_error("Error mallocing: philo");
+	__init__philo(philo, rules->nb_philo, prog);
+	prog->philo = philo;
+	prog->rules = rules;
+	c_time = __init__time(prog);
+	if (c_time)
+		handle_error("Error getting the time");
+	if (c_time || !philo || !rules || !create_size_t(prog))
+		return (1);
+	return (0);
+}
