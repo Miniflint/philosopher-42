38c783f0d81fcf9f581633770e482d236b2e5bd0
miniflint <tristan.goel@gmail.com>
2022-11-21 15:40:25 +0000

    Added bonus (not yet over)

---
+++ philo_bonus/inc/philo.h
@@ -0,0 +1,18 @@
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <sys/time.h>
+# include <sys/wait.h>
+# include <signal.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <pthread.h>
+# include <semaphore.h>
+# include <fcntl.h>
+# include <sys/stat.h>
+
+# include "philostruct.h"
+
+#endif
---
+++ philo_bonus/inc/philostruct.h
@@ -0,0 +1,36 @@
+#ifndef PHILOSTRUCT_H
+# define PHILOSTRUCT_H
+
+# include "philo.h"
+
+typedef struct S_philo
+{
+	int		id;
+	int		ate;
+	long long	last_meal;
+	sem_t		fork_right;
+	sem_t		*fork_left;
+	struct S_prog	*prog;
+}	t_philo;
+
+typedef struct S_rules
+{
+	int	nb_philo;
+	int	time_eat;
+	int	time_death;
+	int	nb_eat;
+	int	died;
+	int	add_max_eat;
+}	t_rules;
+
+typedef struct S_prog
+{
+	
+	long long	first_time;
+	t_rules		rules;
+	sem_t		*writing;
+	t_philo		*philo;
+}	t_prog;
+
+
+#endif
