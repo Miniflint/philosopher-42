f894dc2754fafe49d4f1ca2a14d72aaa697153c9
Tristan Goel <tgoel@c1r3s6.42lausanne.ch>
2022-11-23 17:57:52 +0100

    commmit

--- philo_bonus/src/utils/init.c
+++ philo_bonus/src/utils/init.c
@@ -26,32 +26,6 @@ void	destroy_all(t_prog *prog)
 		kill(prog->philo[i++].pid, SIGKILL);
 	free(prog->philo);
 }
-int __init__sema(t_prog *prog)
-{
-    sem_unlink("/philo_forks");
-    sem_unlink("/philo_write");
-    sem_unlink("/philo_death");
-    sem_unlink("/philo_stop");
-    prog->forks = sem_open("/philo_forks", O_CREAT, 0666, prog->rules.nb_philo);
-    prog->write = sem_open("/philo_write", O_CREAT, 0666, 1);
-    prog->death = sem_open("/philo_death", O_CREAT, 0666, 1);
-    prog->stop = sem_open("/philo_stop", O_CREAT, 0666, 1);
-    return (0);
-}
-
-void	destroy_all(t_prog *prog)
-{
-	int	i;
-
-	i = 0;
-	sem_close(prog->death);
-	sem_close(prog->write);
-	sem_close(prog->stop);
-	sem_close(prog->forks);
-	while (i < prog->rules.nb_philo)
-		kill(prog->philo[i++].pid, SIGKILL);
-	free(prog->philo);
-}
 
 
 static void __init__philo(t_prog *prog)
@@ -89,6 +63,6 @@ int    __init__(t_prog *prog, char **argv, int max_eat)
     prog->rules.add_max_eat = max_eat;
     if (__init__rules(&prog->rules, argv))
         return (1);
-    prog->philo = malloc(sizeof(t_prog) * prog->rules->nb_philo);
+    prog->philo = malloc(sizeof(t_prog) * prog->rules.nb_philo);
     return (0);
 }
